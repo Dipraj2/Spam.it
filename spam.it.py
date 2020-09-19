1ad87479a0a4fe966db5a5477f9eb0f22721b1e6
LuciferDJ <68478921+Dipraj2@users.noreply.github.com>
2020-09-20 01:58:31 +0600

    Update spam.it.py

--- spam.it.py
+++ spam.it.py
@@ -10,7 +10,7 @@ comments2 = input("What you want to comment2: ")
 comments = [comments1,comments2]
 s = int(input("Enter time to wait after a comment (in second) :"))
 print("You have 60 second to open the sight and click on the target comment!\nPress ctrl+x to stop it.")
-time.sleep(6)
+time.sleep(60)
 
 for i in range(x):
     pyautogui.typewrite(comments[i%2])
